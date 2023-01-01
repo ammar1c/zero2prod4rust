759f56f7503a2bd278f09455a819acd721cde6d3
Ammar Al-Batool <ammara@spotify.com>
2023-01-01 19:39:08 +0100

    fix test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::format;
+use sqlx::PgPool;
 use zero2prod::run;
 use zero2prod::configuration;
 
@@ -6,8 +6,11 @@ use zero2prod::configuration;
 async fn main() -> std::io::Result<()> {
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let listener =
         std::net::TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,9 @@
 
 use actix_web::{HttpResponse, web};
 use serde::Deserialize;
-
+use sqlx::PgPool;
+use chrono::Utc;
+use uuid::Uuid;
 
 #[derive(Deserialize, Debug)]
 pub struct SubscribeData {
@@ -9,7 +11,18 @@ pub struct SubscribeData {
     pub email: String,
 }
 
-pub async fn subscribe(form: web::Form<SubscribeData>) -> HttpResponse {
+pub async fn subscribe(form: web::Form<SubscribeData>,
+        pool: web::Data<PgPool>) -> HttpResponse {
     println!("received form {:?}", form);
+    sqlx::query!(
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+        .execute( pool.get_ref())
+        .await
+        .expect("Failed to execute query.");
     HttpResponse::Ok().finish()
 }
